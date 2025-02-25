c8cc3cfa2747dfea62c02748211dd94223cf88e2
ChristopherCaysido <christopher.caysido@gmail.com>
2025-02-25 20:11:08 +0800

    refactor: refactored for creating a todo item in a function
    - Developed the createTodo function, it takes a string input

--- src/main.js
+++ src/main.js
@@ -5,23 +5,37 @@ todoForm.addEventListener('submit', function(event){
   event.preventDefault()
   
   const todoInput = document.getElementById('todo-input')
+  const todoInputText = todoInput.value
+  createTodo(todoInputText)
   // Create an element to house the todo item
-  const todoItem = document.createElement('div')
+  console.log('New todo:', todoText)  
+})
+
+// TODO: Refactor code - Create a function that creates the todo
+
+function createTodo(todo){
   // Get the todo container that will house the todo item
   const todoContainer = document.getElementById('todo-list')
-  // Create the text item inside the todo item
+  
+  // Create the container for the todo
+  const todoItem = document.createElement('div')
+  
+  // Create the name of the todo-itm 
   const todoName = document.createElement('p')
+  
+  // Create the button
+  const todoDoneButton = document.createElement('button')
+  
+  // Inner Text of the Done Button
+  todoDoneButton.innerText = "Task Done"
+  
   // Get the value of the todo
-  const todoText = todoInput.value
+  const todoText = todo
+  
   // use the textContent attribute to add the value of the todo input
   todoName.textContent = todoText
   // append the todoName inside the todoItem
   todoItem.appendChild(todoName)
+  todoItem.appendChild(todoDoneButton)
   todoContainer.appendChild(todoItem)
-  
-  
-  // Create the todo item name
-  
-  console.log('New todo:', todoText)  
-})
-
+}
